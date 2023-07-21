2c0f5303b2b334688dfb3c644f26a68b39aced5d
popoxta <popoxta@gmail.com>
2023-07-21 18:05:47 +1200

    Add button

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,9 +1,12 @@
 export default function TitleCard() {
-    return(
+    return (
         <main>
-           <h1>Hi, I'm <span>Lari</span></h1>]
-            <p>definition (noun)</p>
-            <p>Software developer and lover of simple things like bunnies and stars</p>
+            <section>
+                <h1>Hi, I'm <span>Lari</span></h1>]
+                <p>definition (noun)</p>
+                <p>Software developer and lover of simple things like bunnies and stars</p>
+            </section>
+            <button>Launch</button>
         </main>
     )
 }

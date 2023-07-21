b67e32034e2a00d4803489cda923a84bfab09e40
popoxta <popoxta@gmail.com>
2023-07-21 18:10:10 +1200

    Add article element

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,28 +1,34 @@
 export default function About() {
     return (
         <section>
-            <h2>It's nice to meet you.</h2>
-            <p>
-                My name’s Lari, and I am a creative artist and software developer based in Aotearoa New Zealand.
-            </p>
-            <p>
-                I enjoy building memorable, fun and meaningful projects, and have a strong focus on making all things
-                simple, as they should be.
-            </p>
-            <p>
-                My day-to day sees a wide range of technology, however my main tools of trade are Typescript, React,
-                Tailwind, Node and MongoDB.
-            </p>
-            <p>
-                When not programming, you may find me listening to 80’s rock, making a good family Raclette and taking
-                anyone I can convince to go on a 5 hour walk. Probably longer.
-            </p>
-            <p>
-                Want to know more or grab a coffee? Feel free to
-                <a href="#contact">
-                    get in touch.
-                </a>
-            </p>
+
+            <article>
+                <h2>It's nice to meet you.</h2>
+                <p>
+                    My name’s Lari, and I am a creative artist and software developer based in Aotearoa New Zealand.
+                </p>
+                <p>
+                    I enjoy building memorable, fun and meaningful projects, and have a strong focus on making all
+                    things
+                    simple, as they should be.
+                </p>
+                <p>
+                    My day-to day sees a wide range of technology, however my main tools of trade are Typescript, React,
+                    Tailwind, Node and MongoDB.
+                </p>
+                <p>
+                    When not programming, you may find me listening to 80’s rock, making a good family Raclette and
+                    taking
+                    anyone I can convince to go on a 5 hour walk. Probably longer.
+                </p>
+                <p>
+                    Want to know more or grab a coffee? Feel free to
+                    <a href="#contact">
+                        get in touch.
+                    </a>
+                </p>
+            </article>
+
         </section>
     )
 }

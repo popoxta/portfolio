20488f48fb2f6cdc8317f4802d57ad1989a1901a
popoxta <popoxta@gmail.com>
2023-07-22 19:28:13 +1200

    Render menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import TitleCard from "./components/TitleCard.tsx";
 import About from "./components/About.tsx";
+import Menu from "./components/Menu.tsx";
 
 export default function App() {
     return (
         <>
+            <Menu/>
             <TitleCard/>
             <About/>
         </>

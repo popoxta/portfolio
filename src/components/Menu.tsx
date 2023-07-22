1c0dcbb67a9c2712b2bcb59d6aa9329235ed9958
popoxta <popoxta@gmail.com>
2023-07-22 19:27:42 +1200

    Add menu content

---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+    return(
+        <nav>
+            <a href={'#'}>About</a>
+            <a href={'#'}>Projects</a>
+            <a href={'#'}>Contact</a>
+        </nav>
+    )
+}

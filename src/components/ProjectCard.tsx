fe06c0b7d19da6b0b3b7e2314ad6256a2d60d08f
popoxta <popoxta@gmail.com>
2023-07-23 17:27:28 +1200

    Add ProjectCard.tsx

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,12 @@
+export default function ProjectCard() {
+    return (
+        <div>
+            <h3>Project1</h3>
+            <img src={'#'} alt={''}/>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et minima non odit? Ab eos ex
+                inventore necessitatibus, nemo nisi!
+            </p>
+        </div>
+    )
+}

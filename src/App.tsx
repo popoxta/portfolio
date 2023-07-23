c7d8176376a457aced4a0e2368bf2f826108293f
popoxta <popoxta@gmail.com>
2023-07-23 17:26:54 +1200

    Render projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import TitleCard from "./components/pages/TitleCard.tsx";
 import About from "./components/pages/About.tsx";
 import Menu from "./components/Menu.tsx";
+import Projects from "./components/pages/Projects.tsx";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Menu/>
             <TitleCard/>
             <About/>
+            <Projects/>
         </>
     )
 }

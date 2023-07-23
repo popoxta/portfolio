287181d1e050d993a33296fc03429e750b38d9b7
popoxta <popoxta@gmail.com>
2023-07-23 17:27:50 +1200

    Render project cards in Projects

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,7 +1,12 @@
+import ProjectCard from "../ProjectCard.tsx";
+
 export default function Projects() {
     return (
         <section>
             <h2>Projects</h2>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
         </section>
     )
 }

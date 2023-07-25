a9458434a33ab4572cae6f5f07d7f0663679e8a2
popoxta <popoxta@gmail.com>
2023-07-25 17:34:08 +1200

    Add stub content for contact

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,8 +1,11 @@
-export default function Contact(){
+export default function Contact() {
     return (
         <section>
             <address>
-                TBA
+                <a href="mailto:larissaruecker7@gmail.com">Email</a>
+                <a href="tel:">Phone</a>
+                <a href="#">GitHub</a>
+                <a href="#">LinkedIn</a>
             </address>
         </section>
     )

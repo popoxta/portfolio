c7d9a7e6a24139f1c942f34a322d957207698d74
popoxta <popoxta@gmail.com>
2023-07-26 18:07:05 +1200

    Add styles for main

--- src/components/pages/TitleCard.tsx
+++ src/components/pages/TitleCard.tsx
@@ -1,12 +1,12 @@
 export default function TitleCard() {
     return (
-        <main>
-            <section>
-                <h1>Hi, I'm <span>Lari</span></h1>]
+        <main className={'h-screen flex justify-start items-center pl-16 relative'}>
+            <section className={'-mt-32'}>
+                <h1>Hi, I'm <span>Lari</span></h1>
                 <p>definition (noun)</p>
                 <p>Software developer and lover of simple things like bunnies and stars</p>
             </section>
-            <button>Launch</button>
+            <button className={'absolute bottom-16 left-1/2 right-1/2'}>Launch</button>
         </main>
     )
 }

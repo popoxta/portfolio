c30628945174eaca5432a1d6db4da6d1c168d488
popoxta <popoxta@gmail.com>
2023-07-26 18:50:40 +1200

    Improve menu gap

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
 export default function Menu() {
     return(
-        <nav className={'absolute'}>
+        <nav className={'absolute flex flex-col gap-5'}>
             <a href={'#'}>About</a>
             <a href={'#'}>Projects</a>
             <a href={'#'}>Contact</a>

381df7fcb72cb49b34631ebd121657fa608001ee
popoxta <popoxta@gmail.com>
2023-07-26 18:24:00 +1200

    Add styles for name h1

--- src/components/pages/TitleCard.tsx
+++ src/components/pages/TitleCard.tsx
@@ -1,12 +1,18 @@
 export default function TitleCard() {
     return (
-        <main className={'h-screen flex justify-start items-center pl-16 relative'}>
+        <main className={'h-screen flex justify-start items-center relative p-6 sm:pl-16 sm:p-0 '}>
             <section className={'-mt-32'}>
-                <h1>Hi, I'm <span>Lari</span></h1>
+                <h1 className={'flex flex-row'} style={{fontSize: '1.46rem'}}>
+                    <span className={'flex flex-col mr-3'}>
+                        <span>Hi,</span>
+                        <span>I'm</span>
+                    </span>
+                    <span className={'text-7xl uppercase font-bold'}>Lari</span>
+                </h1>
                 <p>definition (noun)</p>
                 <p>Software developer and lover of simple things like bunnies and stars</p>
             </section>
-            <button className={'absolute bottom-16 left-1/2 right-1/2'}>Launch</button>
+            <button className={'absolute bottom-16 left-0 right-0 mx-auto'}>Launch</button>
         </main>
     )
 }

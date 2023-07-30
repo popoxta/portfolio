e05ddf684630b7f3b65e04f0e8e1e7b7fc8bdc2b
popoxta <popoxta@gmail.com>
2023-07-30 13:23:01 +1200

    Replace hanging-stars.png with svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ export default {
       },
       backgroundImage: { // light -> dark
         'gradient-radial': 'radial-gradient(circle at bottom right, #946086 5%, #745783 15%, #474989 30%, #324072, #253B6B, #163361, #102E5B, #0B2B57, #062750, #03244B)'
+      },
+      spacing: {
+        '1/10': '10%',
       }
     },
   },

dc12bbd272020deb65d7e3c1e06ad7f662c63bfc
popoxta <popoxta@gmail.com>
2023-08-01 07:52:58 +1200

    Add styles for project header

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,12 +1,15 @@
-import ProjectCard from "../ProjectCard.tsx";
-
 export default function Projects() {
+    // const projectNames: string[] = ['name 1', 'name 2', 'name 3', 'name 4']
+
     return (
-        <section>
-            <h2>Projects</h2>
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
+        <section className={'relative min-h-screen flex justify-center place-items-center p-5 mt-10 sm:mt-0'}>
+            <div className={'flex flex-col'}>
+                <h2 className={'text-5xl font-bold mb-10 text-lari-blue text-center sm:text-left'}>
+                    Projects
+                </h2>
+                <div className={'bg-lari-light-blue h-0.5 w-96 -mt-11 -z-10'}></div>
+
+            </div>
         </section>
     )
 }

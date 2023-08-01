7387cdbee70014ccb3c015809034825023f5c64a
popoxta <popoxta@gmail.com>
2023-08-01 18:48:44 +1200

    Add base ProjectCard styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,12 +1,23 @@
 export default function ProjectCard() {
     return (
-        <div>
-            <h3>Project1</h3>
-            <img src={'#'} alt={''}/>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et minima non odit? Ab eos ex
-                inventore necessitatibus, nemo nisi!
-            </p>
+        <div className={'relative w-9/10 h-96 mx-auto'}>
+            <div
+                className={'absolute opacity-30 right-0 bg-lari-light-blue w-9/10 h-full -z-10'}>
+            </div>
+            <div className={'flex h-full gap-10 place-items-center'}>
+                <img
+                    className={'h-80 w-96 object-cover shrink-0'}
+                    src="https://images.unsplash.com/photo-1690228835779-8482c60093bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                    alt={''}
+                />
+                <div className={'flex h-full py-10 flex-col gap-5 justify-between'} >
+                    <h3 className={'text-3xl font-bold'}>Project</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et minima non odit? Ab eos ex
+                        inventore necessitatibus, nemo nisi!
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,24 +1,29 @@
+import ProjectCard from "../ProjectCard.tsx";
+
 export default function Projects() {
     const projectNames: string[] = ['name 1', 'name 2', 'name 3', 'name 4']
 
     const projectNamesRendered = projectNames.map(proj => {
         return (
-            <h3 className={'font-bold text-sm text-lari-blue'}>
+            <h3 key={proj} className={'font-bold text-sm text-lari-blue'}>
                 {proj}
             </h3>
         )
     })
 
     return (
-        <section className={'relative min-h-screen flex justify-center place-items-center p-5 mt-10 sm:mt-0'}>
-            <div className={'flex flex-col'}>
-                <h2 className={'text-5xl font-bold text-lari-blue text-center sm:text-left'}>
-                    Projects
-                </h2>
-                <div className={'bg-lari-light-blue h-0.5 w-96 -mt-1 -z-10'}></div>
-                <div className={'flex gap-10 mt-7'}>
-                    {projectNamesRendered}
+        <section className={'relative min-h-screen flex flex-col justify-center place-items-center p-5 mt-10 sm:mt-0'}>
+            <div className={'max-w-5xl'}>
+                <div className={'mb-10'}>
+                    <h2 className={'text-5xl font-bold text-lari-blue text-center sm:text-left'}>
+                        Projects
+                    </h2>
+                    <div className={'bg-lari-light-blue h-0.5 w-96 -mt-1 -z-10'}></div>
+                    <div className={'flex gap-10 mt-5 ml-1'}>
+                        {projectNamesRendered}
+                    </div>
                 </div>
+                <ProjectCard/>
             </div>
         </section>
     )

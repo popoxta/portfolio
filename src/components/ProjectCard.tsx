49d0fdfdad13fccac5fb323fcf640d557ef214d8
popoxta <popoxta@gmail.com>
2023-08-01 19:21:21 +1200

    Add icons

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,7 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faFigma} from "@fortawesome/free-brands-svg-icons"
+import {faUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+
 export default function ProjectCard() {
     return (
         <div className={'relative w-9/10 h-96 mx-auto'}>
@@ -10,12 +14,17 @@ export default function ProjectCard() {
                     src="https://images.unsplash.com/photo-1690228835779-8482c60093bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
                     alt={''}
                 />
-                <div className={'flex h-full py-10 flex-col gap-5 justify-between'} >
-                    <h3 className={'text-3xl font-bold'}>Project</h3>
-                    <p>
+                <div className={'flex h-full py-10 pr-2 flex-col gap-5 justify-between'}>
+                    <h3 className={'text-3xl font-bold text-lari-blue sm:text-left'}>Project</h3>
+                    <p className={'text-lari-blue'}>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et minima non odit? Ab eos ex
                         inventore necessitatibus, nemo nisi!
                     </p>
+                    <div className={'flex gap-5 place-items-center'}>
+                        <FontAwesomeIcon size={'2xl'} icon={faGithub} color={'#9D9CE6'}/>
+                        <FontAwesomeIcon size={'xl'} icon={faFigma} color={'#9D9CE6'}/>
+                        <FontAwesomeIcon size={'xl'} icon={faUpRightFromSquare} color={'#9D9CE6'}/>
+                    </div>
                 </div>
             </div>
         </div>

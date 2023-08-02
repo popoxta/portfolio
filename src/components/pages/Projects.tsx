152670b6bc56a30e11ff18c3d9fde2ffddc6d93f
popoxta <popoxta@gmail.com>
2023-08-02 19:09:54 +1200

    Add underline animation

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,29 +1,45 @@
 import ProjectCard from "../ProjectCard.tsx";
+import {ReactElement, useState} from "react";
+import {motion} from "framer-motion";
+import projectsData from "../../data/projectsData.tsx";
 
 export default function Projects() {
-    const projectNames: string[] = ['name 1', 'name 2', 'name 3', 'name 4']
+    const [page, setPage] = useState(0)
 
-    const projectNamesRendered = projectNames.map(proj => {
+    const projectsRendered: ReactElement[] = projectsData.map(({name}: { name: string }, index: number) => {
         return (
-            <h3 key={proj} className={'font-bold text-sm text-lari-blue'}>
-                {proj}
-            </h3>
+            <>
+                <button key={name} className={`relative font-bold text-sm text-lari-blue ${page === index ? '' : ''}`}
+                        onClick={() => selectProject(index)}>
+                    {name}
+                    {index === page && <motion.div className={'select-underline'} layoutId={'underline'}/>}
+                </button>
+
+            </>
         )
     })
 
+    const selectProject = (index: number): void => {
+        setPage(index)
+    }
+
+    const projectCards: ReactElement[] = projectsData.map(project => {
+        return <ProjectCard project={project}/>
+    })
+
     return (
-        <section className={'relative min-h-screen flex flex-col justify-center place-items-center p-5 mt-10 sm:mt-0'}>
+        <section className={'relative min-h-screen flex flex-col justify-center place-items-center p-5 mt-10 lg:mt-0'}>
             <div className={'max-w-5xl'}>
                 <div className={'mb-10'}>
-                    <h2 className={'text-5xl font-bold text-lari-blue text-center sm:text-left'}>
+                    <h2 className={'text-5xl font-bold text-lari-blue text-center lg:text-left'}>
                         Projects
                     </h2>
-                    <div className={'bg-lari-light-blue h-0.5 w-96 -mt-1 -z-10 mx-auto sm:mx-0'}></div>
-                    <div className={'flex gap-10 mt-5 ml-1 justify-center sm:justify-start'}>
-                        {projectNamesRendered}
+                    <div className={'flex gap-10 ml-1 mt-5 justify-center lg:justify-start'}>
+                        {projectsRendered}
                     </div>
                 </div>
-                <ProjectCard/>
+                {projectCards[page]}
+
             </div>
         </section>
     )

34ab2f6306f753f2a87de2c7b9dbbc7363c62ed8
popoxta <popoxta@gmail.com>
2023-08-02 19:09:34 +1200

    Increase media query breakpoints

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,25 +5,25 @@ import {Project} from "../types/project.tsx";
 
 export default function ProjectCard({project}: { project: Project }) {
 
-    const descriptions = project.description.split('.').map((sentence, i) =>
-        <p className={i > 0 ? 'mt-3' : ''}>{sentence}</p>)
+    const descriptions = project.description.split('.').map(sentence =>
+        <p className={'text-sm '}>{sentence}</p>)
+
     return (
-        <div className={'relative flex place-items-center w-9/10 mx-auto sm:min-h-[24rem]'}>
+        <div className={'relative flex place-items-center w-9/10 mx-auto lg:max-w-none md:max-w-3xl '}>
             <div
-                className={'absolute opacity-30 right-0 bg-lari-light-blue -z-10 w-full bottom-0 h-3/4 sm:h-full sm:w-9/10 '}>
+                className={'absolute opacity-30 right-0 bg-lari-light-blue -z-10 w-full bottom-0 h-3/4 lg:h-full lg:w-9/10 '}>
             </div>
-            <div className={'flex h-full place-items-center flex-col sm:flex-row sm:gap-10'}>
+            <div
+                className={'flex h-full min-h-[38rem] place-items-center flex-col lg:min-h-[25rem] lg:flex-row lg:gap-10'}>
                 <img
-                    className={'h-80 object-cover shrink-0 w-9/10 sm:w-96 '}
+                    className={'h-80 object-cover shrink-0 w-9/10 lg:w-96 '}
                     src="https://images.unsplash.com/photo-1690228835779-8482c60093bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
                     alt={''}
                 />
-                <div className={'flex py-10 pr-5 flex-col gap-5 text-center sm:text-left'}>
-                    <h3 className={'text-3xl font-bold text-lari-blue sm:text-left'}>{project.name}</h3>
-                    <p className={'text-lari-blue'}>
-                        {descriptions}
-                    </p>
-                    <div className={'flex gap-5 place-items-center justify-center sm:justify-start'}>
+                <div className={'flex flex-col gap-5 text-center pr-5 pl-5 py-5 lg:pl-0 lg:py-5 lg:text-left'}>
+                    <h3 className={'text-3xl font-bold text-lari-blue lg:text-left'}>{project.name}</h3>
+                    {descriptions}
+                    <div className={'flex gap-5 place-items-center justify-center lg:justify-start'}>
                         {
                             project.links.github &&
                             <a href={project.links.github} className={'hover:brightness-90 transition-all'}>

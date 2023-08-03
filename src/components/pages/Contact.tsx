f74fd0c57b81f8388e3a73758ca0c5bc6f20508d
popoxta <popoxta@gmail.com>
2023-08-03 18:54:34 +1200

    Add contact form styles

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,12 +1,44 @@
 export default function Contact() {
     return (
-        <section>
-            <address>
-                <a href="mailto:larissaruecker7@gmail.com">Email</a>
-                <a href="tel:">Phone</a>
-                <a href="#">GitHub</a>
-                <a href="#">LinkedIn</a>
-            </address>
+        <section className={'min-h-screen relative flex flex-col justify-center place-items-center'}>
+            <div className={'max-w-5xl '}>
+                <h2 className={'text-5xl font-bold text-lari-blue mb-5'}>Let's chat</h2>
+                <p className={'text-lari-blue'}>Want to connect, have a chat about software, my projects, coffee or
+                    simply the weather?</p>
+                <p className={'text-lari-blue'}>Send me a message and let's talk!</p>
+                <div className={'flex gap-10'}>
+                    <form className={'mt-10'}>
+                        <div className={'flex gap-5'}>
+                            <label htmlFor={"name"} className={'flex flex-col text-lari-gray text-sm font-bold'}>Name
+                                <input className={'bg-lari-lighter-blue p-1.5 rounded-md border-2 border-lari-light-blue'}
+                                       type={"text"} id={"name"} name={"name"}/>
+                            </label>
+
+                            <label htmlFor={"email"} className={'flex flex-col text-lari-gray text-sm font-bold'}>Email
+                                <input className={'bg-lari-lighter-blue p-1.5 rounded-md border-2 border-lari-light-blue'}
+                                       type={"text"} id={"email"} name={"email"}/>
+                            </label>
+                        </div>
+                        <label htmlFor={"message"} className={'mt-5 flex flex-col text-lari-gray text-sm font-bold'}>
+                            Message
+                        <textarea className={'bg-lari-lighter-blue p-1.5 rounded-md border-2 border-lari-light-blue'}
+                                  name={"message"} id={"message"} rows={10}></textarea>
+                        </label>
+                        <button type={'submit'}>Submit</button>
+                    </form>
+                    <address>
+                        <div>
+                            <a href={''}>larissaruecker7@gmail.com</a>
+                        </div>
+                        <div>
+                            <a href={''}>popoxta</a>
+                        </div>
+                        <div>
+                            <a href={''}>linkedIn</a>
+                        </div>
+                    </address>
+                </div>
+            </div>
         </section>
     )
 }

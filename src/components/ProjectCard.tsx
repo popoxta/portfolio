bbe89860765281613de62f22d462d49d3c5e88f1
popoxta <popoxta@gmail.com>
2023-08-04 17:28:17 +1200

    Fix p text color

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,7 +6,7 @@ import {Project} from "../types/project.tsx";
 export default function ProjectCard({project}: { project: Project }) {
 
     const descriptions = project.description.split('.').map((sentence, i) =>
-        <p key={i} className={'text-sm '}>{sentence}</p>)
+        <p key={i} className={'text-sm text-lari-blue'}>{sentence}</p>)
 
     return (
         <div className={'relative flex place-items-center w-9/10 mx-auto lg:max-w-none md:max-w-3xl '}>

cafcaf1c74dc5ee544ed635d68ba98822af3f5bd
popoxta <popoxta@gmail.com>
2023-08-04 17:50:23 +1200

    Fix z-index

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,15 +10,13 @@ export default function ProjectCard({project}: { project: Project }) {
 
     return (
         <div className={'relative flex place-items-center w-9/10 mx-auto lg:max-w-none md:max-w-3xl '}>
-            <div
-                className={'absolute right-0 bg-lari-lighter-blue -z-10 w-full bottom-0 h-3/4 lg:h-full lg:w-9/10 '}>
+            <div className={'absolute right-0 bg-lari-lighter-blue -z-10 w-full bottom-0 h-3/4 lg:h-full lg:w-9/10 '}>
             </div>
             <div
                 className={'flex h-full min-h-[38rem] place-items-center flex-col lg:min-h-[25rem] lg:flex-row lg:gap-10'}>
                 <img
-                    className={'h-80 object-cover shrink-0 w-9/10 lg:w-96 '}
+                    className={'h-80 object-cover shrink-0 w-9/10 lg:w-96 '} alt={''}
                     src="https://images.unsplash.com/photo-1690228835779-8482c60093bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-                    alt={''}
                 />
                 <div className={'relative flex flex-col gap-5 text-center pr-5 pl-5 py-5 lg:pl-0 lg:py-5 lg:text-left'}>
                     <h3 className={'text-3xl font-bold text-lari-blue lg:text-left'}>{project.name}</h3>
--- src/components/pages/TitleCard.tsx
+++ src/components/pages/TitleCard.tsx
@@ -15,7 +15,7 @@ export default function TitleCard() {
         <main id={'home'} className={'h-screen p-5 sm:p-12 bg-lari-blue bg-gradient-radial '}>
             <div className={'border-2 p-4 sm:p-0 overflow-hidden h-full relative'}>
                 <motion.section variants={slideRight} initial={'hidden'} whileInView={'show'}
-                                className={'h-full flex justify-start items-center relative '}>
+                                className={'h-full flex justify-start items-center relative z-50'}>
                     <div className={'sm:pl-16'}>
                         <motion.div>
                             <h1 className={'flex flex-row sm:text-4xl'}>
@@ -33,19 +33,18 @@ export default function TitleCard() {
                         </motion.div>
                     </div>
                 </motion.section>
-                <motion.div variants={slideDown} initial={'hidden'} whileInView={'show'}>
-                    <motion.div className={'mx-auto absolute w-fit bottom-5 left-0 right-0 sm:bottom-11'}
-                                variants={slideDown}>
+                    <motion.div className={'mx-auto absolute w-fit bottom-5 left-0 right-0 sm:bottom-11 z-50'}
+                variants={slideDown} initial={'hidden'} whileInView={'show'} >
                         <button
-                            className={'launch text-sm text-white hover:text-lari-light-blue transition-colors'}>
-                            Launch
+                            className={'launch text-sm text-white hover:text-lari-light-blue transition-colors cursor-pointer'}>
+                            <a href="#about">Launch</a>
                         </button>
                     </motion.div>
                     <motion.div className={' mx-auto absolute w-fit bottom-14 left-0 right-0 sm:bottom-20 '}
-                                variants={slideDown}>
+                                variants={slideDown} initial={'hidden'} whileInView={'show'}>
                         <div className={'launch-bar w-px bg-white h-12 sm:h-16 transition-transform'}></div>
                     </motion.div>
-                </motion.div>
+
                 <Particles className={'absolute w-full h-full top-0'} id={'tsparticles'}
                            init={particlesInit} options={{
                     'particles': {

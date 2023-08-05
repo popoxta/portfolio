40a47e91398c64a1638b46463677222c92f56ad8
lari <popoxta@gmail.com>
2023-08-05 00:28:44 -0700

    Fix linkedin link

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -71,7 +71,7 @@ export default function Contact() {
                         </div>
                         <div className={'hover:brightness-90 transition-all ease-in-out'}>
                             <FontAwesomeIcon icon={faGithub} size={'2xl'} className={'mr-5'} color={'#afaee3'}/>
-                            <a href={''} target={'_blank'} className={'text-lari-gray text-sm font-bold not-italic'}>popoxta</a>
+                            <a href={'https://www.linkedin.com/in/larissa-ruecker/'} target={'_blank'} className={'text-lari-gray text-sm font-bold not-italic'}>popoxta</a>
                         </div>
                         <div className={'hover:brightness-90 transition-all ease-in-out'}>
                             <FontAwesomeIcon icon={faLinkedin} size={'2xl'} className={'mr-5'} color={'#afaee3'}/>
